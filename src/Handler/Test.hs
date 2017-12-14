09ce665136b44e5c98de67e24e5bd2106f5cd849
Steven Leiva <leiva.steven@gmail.com>
2017-12-13 20:51:51 -0500

    Test setting and getting messages.

--- src/Handler/Test.hs
+++ src/Handler/Test.hs
@@ -9,17 +9,8 @@ module Handler.Test where
 
 import Import
 
-myWidget1 :: WidgetT App IO () -- Widget would have also worked!
-myWidget1 = do
-    toWidget [hamlet|<h1>A Title|]
-    toWidget [lucius|h1 {color: green}|]
-
-myWidget2 :: Widget
-myWidget2 = do
-    setTitle "My Page Title"
-
-getTestR :: Handler Html
-getTestR =
-    defaultLayout $ do
-        myWidget1
-        myWidget2
+getTestR :: Handler ()
+getTestR = do
+    setMessage $
+        toHtml ("Hello. I am testing setting / getting messages" :: String)
+    redirect HomeR
