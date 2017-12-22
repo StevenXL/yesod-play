14e0977e896afeeb504170b4ffc8670343ca3275
Steven Leiva <leiva.steven@gmail.com>
2017-12-22 13:31:49 -0500

    Compose validations on one field.

--- src/Forms/Person.hs
+++ src/Forms/Person.hs
@@ -13,13 +13,28 @@ import Types.Person (Person(..))
 -- some fields
 nameField = textField
 
-ageField = check validateYear intField
+ageField = check validateYear $ checkM validateNotInFuture intField
 
 -- some validations
 validateYear :: (Ord a, Num a) => a -> Either Text a
 validateYear year
     | year < 1990 = Left "You are too old!"
     | otherwise = Right year
+
+validateNotInFuture :: Int -> Handler (Either Text Int)
+validateNotInFuture year = do
+    thisYear <- liftIO getCurrrentYear
+    return $
+        if year > thisYear
+            then Left "You have a time machine"
+            else Right year
+
+getCurrrentYear :: IO Int
+getCurrrentYear = do
+    now <- getCurrentTime
+    let today = utctDay now
+        (year, _, _) = toGregorian today
+    return $ fromInteger year
 
 -- some forms
 nameForm = areq nameField "Name" Nothing
